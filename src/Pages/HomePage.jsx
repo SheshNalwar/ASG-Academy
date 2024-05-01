89fd2402591c4ee98dc8a3a40ece013b156bf331
Vaishnavi-Patil-29 <vaishnavipatiled@gmail.com>
2024-05-01 23:27:22 +0530

    Homepage changes-1

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,22 +1,37 @@
-import "../css/homepage.css" 
+import "../css/homepage.scss" 
+
 const HomePage = () => {
   return (
-    <>
-     <section className="home">
-        <div className="Home-content">
-          <h3>ASG Academy</h3>
-          <h1>hello its me</h1>
-          <h2>welcome</h2>
-          <p>The world-leading platform with successful lessons.</p>
-          <div className="home-sci">
-
-          </div>
-
-        </div>
-      </section>
+    <> 
+    <div className="Hompage">
+      <img className="Homepage-1-child" alt="" src="alex.jpg" />
+      <img className="Homepage-2-child" alt="" src="monica.jpg" />
+      <img className="Homepage-3-child" alt="" src="darshan.jpg" />
+      <img className="Homepage-4-child" alt="" src="chris.jpg" />
+      <div className="Homepage-1-item" />
+      <input className="search-course"></input>
+      <img className="search-icon" alt="" src="icons8-search-56.jpg" />
+      <div className="circle-1-child" ></div>
+      <div className="circle-2-child" ></div>
+      <div className="circle-3-child" ></div>
+      <div className="circle-4-child" ></div>
+    </div>
+    <div className="Home-content">
+          <h3 id="text1">Welcome To</h3>
+          <h3 id="heading">ASG Academy</h3>
+          <h1 id="text2">Learn With The Best School.</h1>
+          <p>The world-leading platform with successful lessons.</p> 
+    </div>
    
+
     </>
   );
 };
 
 export default HomePage;
+
+
+
+
+
+
