7fc799c1f392fee5c3fac1690dd6611904081a58
SheshNalwar <nalwarshesh@gmail.com>
2024-05-01 23:46:23 +0530

    Padding in gallery page

--- src/Pages/GalleryPage.jsx
+++ src/Pages/GalleryPage.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { client } from '../schema.js';
-
+import "../css/App.scss"
 const GalleryPage = () => {
   const [data, setData] = useState([]);
 
