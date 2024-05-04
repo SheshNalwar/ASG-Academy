daf7f9c4fa3518f213a6ac6f135ad917d1529643
Umesh Nagare <umeshnagare27@gmail.com>
2024-05-04 11:26:02 +0530

    Gallery Page changes

--- src/Pages/GalleryPage.jsx
+++ src/Pages/GalleryPage.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
-import { client } from '../utils/schema.js';
-import "../css/GalleryPage.scss"
+import { client } from "../utils/schema.js";
+import "../css/GalleryPage.scss";
 const GalleryPage = () => {
   const [data, setData] = useState([]);
 
@@ -29,16 +29,23 @@ const GalleryPage = () => {
   return (
     <div className="galleryPage">
       {/* <div className="bgdiv"></div> */}
-      <h1 className="galleryTitle">Images from our Academy</h1>
+      <h1 className="galleryTitle">
+        Images from our <span>Academy</span>
+      </h1>
       <div className="galleryContainer">
-        {data && data.map((image, index) => (
-          <div className="galleryImage" key={index}>
-            <h3 className="imgTitle">{image.title}</h3>
-            <img src={image.mainImage.asset.url} alt={image.mainImage.alt} className="image" />
-          </div>
-        ))}
+        {data &&
+          data.map((image, index) => (
+            <div className="galleryImage" key={index}>
+              <h3 className="imgTitle">{image.title}</h3>
+              <img
+                src={image.mainImage.asset.url}
+                alt={image.mainImage.alt}
+                className="image"
+              />
+            </div>
+          ))}
       </div>
-    </div >
+    </div>
   );
 };
 
